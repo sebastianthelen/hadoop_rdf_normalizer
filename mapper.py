3c9fd12332afdf2b6d938ff13d565efeecfe6a72
Sebastian Thelen <sebastianthelen@gmail.com>
2015-05-18 17:33:56 +0200

    first (almost) working version

--- mapper.py
+++ mapper.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python
 import sys
 from rdflib.graph import Graph
+from rdflib import Literal
+import rdflib
 from io import StringIO
 
+# read input file in ntriple format
 def read_input(file):
     for line in file:
-        # split the line into words
         yield line
 
-def main():
-    data = read_input(sys.stdin)
-    for triple in data:
+
+def map():
+    ntriples = read_input(sys.stdin)
+    for triple in ntriples:
+        # get rid of blank lines
         triple = triple.replace('.\n', '.')
-        flo = StringIO(triple.decode('utf-8'))
+        # load triples into rdflib model
+        # --> easier processing
         graph = Graph()
-        graph.parse(flo, format="nt")
+        graph.parse(StringIO(triple.decode('utf-8')),
+                    format="nt")
+        # there is actually just one triple
+        # in the graph
         for subject, predicate, obj in graph:
-            print ('%s\t%s' % (obj, triple))
+            # <key, value> = <obj, triple>
+            if isinstance(obj, rdflib.term.Literal):
+                print u'"%s"^^<%s>\t%s' % (obj,
+                                          Literal(obj.datatype),
+                                          triple)
+            else:
+                print '<%s>\t%s' % (obj, triple)
+
 
 if __name__ == "__main__":
-    main()
+    map()
--- reducer.py
+++ reducer.py
@@ -1,49 +1,83 @@
 #!/usr/bin/env python
 
-from itertools import groupby
-from operator import itemgetter
 import sys
-from rdflib.term import URIRef
+from rdflib import OWL
 from rdflib.graph import Graph
+from rdflib import Literal
+import rdflib
 from io import StringIO
 
+# input for the reducer is a set of <obj, triple>
+# key/value pairs ordered by obj identifiers
 def reduce():
-    # input comes from STDIN
-    key = None
+    # input comes from stdin
+    previous_key = None
     graph = None
+    # load a triples having same previous_key into a model
     for line in sys.stdin:
-        # remove leading and trailing whitespace
         line = line.strip()
-        # parse the input we got from mapper.py
-        print(line.split('\t',1))
-        current_key, value = line.split('\t',1)
-        if(current_key == key):
-            # create file like object
-            print 'xxxx'
-            flo = StringIO(value)
-            # load flo into temporary graph
+        current_key, triple = line.split('\t', 1)
+        if current_key == previous_key or graph is None:
+            if graph is None:
+                graph = Graph()
+            # load triple triple into temp graph
+            # for easier processing
             tmp_graph = Graph()
-            tmp_graph.parse(flo)
+            tmp_graph.parse(StringIO(triple.decode('utf-8')),
+                            format="nt")
             # get triple from temporary graph
-            for subject, predicate, obj in tmp_graph:
+            for sub, pred, obj in tmp_graph:
                 # add to current graph
-                graph.add(subject,predicate,object)
-        if(graph == None):
-            graph = Graph()
-            key = current_key
-
+                graph.add((sub, pred, obj))
         else:
-            replace_object_uri(graph)
-            key = current_key
+            replaceObjectUri(graph)
             graph = Graph()
+            # load triple triple into temp graph
+            # for easier processing
+            tmp_graph = Graph()
+            tmp_graph.parse(StringIO(triple.decode('utf-8')),
+                            format="nt")
+            # get triple from temporary graph
+            for sub, pred, obj in tmp_graph:
+                # add to current graph
+                graph.add((sub, pred, obj))
+        previous_key = current_key
+    replaceObjectUri(graph)
 
 
+def formatAsUri(resource):
+    if isinstance(resource, rdflib.term.Literal):
+        return '"%s"^^<%s>' % (resource, Literal(resource.datatype))
+    else:
+        return '<%s>' % resource
 
-def replace_object_uri(graph):
-    for subject, predicate, object in graph:
-       res = graph.triples(None,OWL.sameAs,object)
-       if (len(res)>0):
-           print(subject,predicate,res)
+
+def replaceObjectUri(graph):
+    flag = False
+    for subj, pred, obj in graph:
+        # determine obj's cellar id, if it exists (owl:sameAs)
+        results = graph.triples((None, OWL.sameAs, obj))
+        sameas_stmnt = None
+        try:
+            # owl:sameAs statement with cellar id as subject
+            sameas_stmnt = results.next()
+        except StopIteration:
+            # no owl:sameAs statement found
+            pass
+        # nothing to replace
+        if sameas_stmnt is None:
+            print formatAsUri(subj), formatAsUri(pred), formatAsUri(obj)
+        # replace obj by cellar_id
+        else:
+            sameas_subj = sameas_stmnt[0]
+            # filter reflexive statements
+            if str(sameas_subj) != str(subj) and str(pred) != str(OWL.sameAs):
+                print formatAsUri(subj), formatAsUri(pred), formatAsUri(sameas_subj)
+            # original owl:sameAs statement should be printed once
+            if not(flag):
+                print formatAsUri(subj), formatAsUri(OWL.sameAs), formatAsUri(obj)
+                flag = True
+
 
 if __name__ == "__main__":
     reduce()
